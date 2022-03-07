e6861fdf2e2a0b5023345a34f5e75579794102cf
Rob Hess <hessro@oregonstate.edu>
2022-03-07 14:42:48 -0800

    Add code from 3/7 lecture.

--- src/pages/api/tokenExchange.js
+++ src/pages/api/tokenExchange.js
@@ -1,3 +1,35 @@
-export default (req, res) => {
-  res.status(200).send({ msg: "OK!" });
+import fetch from 'isomorphic-unfetch';
+
+import {
+  generateAuthToken,
+  setAuthCookie
+} from '../../lib/auth';
+
+export default async (req, res) => {
+  const { code } = req.body;
+  if (!code) {
+    res.status(400).send({ err: "Must specify authorization code."})
+  } else {
+    const githubRes = await fetch(
+      'https://github.com/login/oauth/access_token',{
+        method: 'POST',
+        body: JSON.stringify({
+          client_id: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code: code
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      });
+      const githubResBody = await githubRes.json();
+      if (githubResBody.access_token) {
+        // Store access_token in server-side cache (e.g. Redis)
+        setAuthCookie(res, generateAuthToken("..."));
+        res.status(200).send({ msg: "OK!" });
+      } else {
+        res.status(401).send({ err: githubResBody.error_description })
+      }
+  }
 }
--- src/pages/github/login.js
+++ src/pages/github/login.js
@@ -1,9 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import GitHubLoginLink from '../../components/GitHubLoginLink';
 
 export default function GitHubLogin() {
+  const router = useRouter();
   const [ error, setError ] = useState("");
+
+  useEffect(() => {
+    async function exchangeForAccessToken(code) {
+      const res = await fetch('/api/tokenExchange', {
+        method: 'POST',
+        body: JSON.stringify({
+          code: code
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const resBody = await res.json();
+      if (res.status !== 200) {
+        setError(resBody.err)
+      } else {
+        console.log("== Auth succeeded!");
+      }
+    }
+    if (router.query.code) {
+      exchangeForAccessToken(router.query.code);
+    }
+  }, [ router.query.code ]);
 
   return (
     <div>
